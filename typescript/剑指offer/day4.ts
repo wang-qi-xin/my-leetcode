d6300a7b7ca070f36df19e3c32100458e2f05892
markqxwang <markqxwang@tencent.com>
2022-04-20 14:11:47 +0800

    剑指 Offer II 074. 合并区间

--- typescript/剑指offer/day4.ts
+++ typescript/剑指offer/day4.ts
@@ -300,3 +300,109 @@ function pathSum(root: TreeNode | null, target: number): number[][] {
   dfs(root, 0)
   return res
 }
+
+/**
+ * 剑指 Offer 51. 数组中的逆序对(困难)
+ * 方法一：暴力法：超时
+ * @param nums
+ */
+function reversePairs(nums: number[]): number {
+  /**
+  对每一位数字，都从该位遍历
+  1. 如果遇到比它小的，res += 1
+   */
+  let res = 0
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] < nums[i]) {
+        res++
+      }
+    }
+  }
+  return res
+}
+/**
+ * 剑指 Offer 51. 数组中的逆序对(困难)
+ * 方法二，动态规划
+ * @param nums
+ */
+function reversePairs2(nums: number[]): number {
+  /**
+  从后向前遍历（或逆转数组）
+  1. 从当前位向后遍历，遇到的第一个比它小的数字j。dp[i] = 1 + j 
+   */
+  let dp = [0]
+  nums.reverse()
+  for (let i = 1; i < nums.length; i++) {
+    let min = i - 1
+    for (let j = i - 1; j >= 0; j--) {
+      if (nums[i] > nums[j]) {
+        min = j
+        break
+      }
+    }
+    if (nums[i] > nums[min]) {
+      console.log(min, nums[min])
+
+      dp[i] = 1 + dp[min]
+    } else {
+      dp[i] = 0
+    }
+  }
+  let res = 0
+  console.log(dp)
+
+  dp.forEach(v => {
+    res += v
+  })
+  return res
+}
+
+// console.log(reversePairs2([7,5,6,4]))
+/**
+ * 剑指 Offer II 074. 合并区间
+以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+
+ 
+
+示例 1：
+
+输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+输出：[[1,6],[8,10],[15,18]]
+解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+示例 2：
+
+输入：intervals = [[1,4],[4,5]]
+输出：[[1,5]]
+解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ 
+
+提示：
+
+1 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 104
+ * @param intervals 
+ */
+function merge(intervals: number[][]): number[][] {
+  intervals.sort((a, b) => {
+    return a[0] - b[0]
+  })
+  const res = [intervals[0]]
+  let num = 0;
+  for(let i = 0; i < intervals.length; i++){
+    if(res[num][1] >= intervals[i][0]){
+      // 重合。
+      res[num][1] = Math.max(res[num][1], intervals[i][1])
+    }else {
+      res[++num] = intervals[i]
+    }
+  }
+  return res
+}
+console.log(
+  merge([
+    [1, 4]
+    
+  ])
+)
