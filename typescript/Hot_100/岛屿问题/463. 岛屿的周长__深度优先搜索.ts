e2b8f7ab80d2f82c7bac082f4a38d755b507a050
markqxwang <markqxwang@tencent.com>
2022-07-01 16:19:43 +0800

    463. 岛屿的周长--迭代

--- typescript/Hot_100/岛屿问题/463. 岛屿的周长__深度优先搜索.ts
+++ typescript/Hot_100/岛屿问题/463. 岛屿的周长__深度优先搜索.ts
@@ -6,11 +6,8 @@
 
 岛屿中没有“湖”（“湖” 指水域在岛屿内部且不和岛屿周围的水相连）。格子是边长为 1 的正方形。网格为长方形，且宽度和高度均不超过 100 。计算这个岛屿的周长。
 
- 
 
 示例 1：
-
-
 
 输入：grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
 输出：16
@@ -74,6 +71,45 @@ function islandPerimeter(grid: number[][]): number {
   console.log(grid)
 
   return 0
+}
+
+function islandPerimeter2(grid: number[][]): number {
+  /**
+   方法一：迭代
+  
+   缺点：只能用于本题，一个岛屿的情况。
+
+   深度优先搜索，可以用于计算多个岛屿的周长。
+   */
+  const m = grid.length,
+    n = grid[0].length,
+    d = [
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1]
+    ]
+  let res = 0
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === 1) {
+        // 1. 如果grid[i][j]是陆地，为结果增加的周长，可能为4
+        res += 4
+        for (let k = 0; k < 4; k++) {
+          const nexti = i + d[k][0],
+            nextj = j + d[k][1]
+          if (nexti >= 0 && nexti < m && nextj >= 0 && nextj < n) {
+            if (grid[nexti][nextj] === 1) {
+              // 2. 如果grid[i][j]四周存在陆地，则与该陆地相邻的边，不为岛屿提供周长1.所以从res中减去
+              res -= 1
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return res
 }
 
 console.log(islandPerimeter([[0, 1]]))
